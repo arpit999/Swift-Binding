e5e6551e683e0cba4613e5ea96269ac75fa26775
arpit <arpitpatel009@gmail.com>
2024-05-20 21:36:16 -0400

    TextField with bind runtime change

--- Swift Binding/ContentView.swift
+++ Swift Binding/ContentView.swift
@@ -2,18 +2,19 @@
 //  ContentView.swift
 //  Swift Binding
 //
-//  Created by Arpit & Jinal on 2024-05-20.
+//  Created by Arpit on 2024-05-20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    // Bind runtime change
+    @State var hello = "Hello World"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Type your name", text: $hello)
+            Text(hello).font(.title)
         }
         .padding()
     }
