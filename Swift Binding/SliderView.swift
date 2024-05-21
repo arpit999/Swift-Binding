f6b86791af5c4cb57da882873bdd2a362c117eb1
arpit <arpitpatel009@gmail.com>
2024-05-20 22:09:56 -0400

    Create reusable slider and tie variable to that UI

---
+++ Swift Binding/SliderView.swift
@@ -0,0 +1,18 @@
+//
+//  SliderView.swift
+//  Swift Binding
+//
+//  Created by Arpit & Jinal on 2024-05-20.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderView()
+}
