218bfd8ca792cd24feab90f4c479984060a8cf17
arpit <arpitpatel009@gmail.com>
2024-05-20 21:10:14 -0400

    Initial Commit

---
+++ Swift Binding/Swift_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_BindingApp.swift
+//  Swift Binding
+//
+//  Created by Arpit & Jinal on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
